23352ec8a331378e860c260b1891ec88c6a4e64d
Vishek Patel <85006315+vishek-patel@users.noreply.github.com>
2023-12-17 18:41:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const quizRoutes = require('./routes/quizRoutes');
-const uploadRoutes = require('./routes/uploadRoutes'); // Assuming you have this route
+const uploadRoutes = require('./routes/uploadRoutes');
 const Quiz = require('./models/Quiz');
 const app = express();
+
+// Enable dotenv configuration
+require('dotenv').config();
 
 app.use(cors());
 app.use(helmet());
@@ -20,22 +23,25 @@ const limiter = rateLimit({
 });
 app.use(limiter);
 
+// Enable trust proxy
+app.set('trust proxy', true);
+
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.error(err));
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.error(err));
 
 // Routes
 app.use('/api/quiz', quizRoutes);
-app.use('/api/upload', uploadRoutes); // Upload route
+app.use('/api/upload', uploadRoutes);
 
 // Health check
 app.get('/health', (req, res) => res.status(200).send('OK'));
 
 // Global error handler
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 const newPort = 4000; // Change to an available port
